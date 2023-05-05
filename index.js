b199119f45ba9ab70fb9ba7711974cbb760257b7
Chris Grimmett <chris@grimtech.net>
2023-05-04 21:53:35 -0800

    progress

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ dotenv.config()
 import { RefreshingAuthProvider } from '@twurple/auth';
 import { ChatClient } from '@twurple/chat';
 import { registerOfferHandler, registerBuyHandler } from './lib/offer.js';
-import { getTwitchTokenData, setTwitchTokenData } from './lib/tokens.js';
+import { getTwitchTokenData, setTwitchTokenData, registerApiConnectTwitch } from './lib/tokens.js';
 import { promises as fs } from 'fs';
 import { registerAuctions } from './lib/auction.js'
+import { registerServerHandler } from './lib/server.js'
 import { getBotUsers, getUser } from './lib/user.js'
 import _ from 'lodash';
 import { got } from 'got';
 import { v2 } from 'cloudinary';
+
 const cloudinary = v2
+
 
 const appEnv = new Array(
   'TWITCH_CLIENT_ID',
@@ -25,6 +28,8 @@ const appEnv = new Array(
   'BACKEND_URL',
   'FRONTEND_URL',
   'BOT_NAME',
+  'BOT_URL',
+  'PORT',
 )
 
 const appContext = {
@@ -52,27 +57,7 @@ const config = cloudinary.config({
 const backend = process.env.BACKEND_URL
 const frontend = process.env.FRONTEND_URL
 
-const authProvider = new RefreshingAuthProvider(
-  {
-    clientId: process.env.TWITCH_CLIENT_ID,
-    clientSecret: process.env.TWITCH_CLIENT_SECRET,
-    onRefresh: async (userId, newTokenData) => {
-      try {
-        await setTwitchTokenData(
-          process.env.BACKEND_URL,
-          process.env.STRAPI_API_KEY,
-          userId,
-          newTokenData,
-        )
-      } catch (e) {
-        console.error('error while refreshing twitch token')
-        console.error(e)
-        console.error(e.options.body)
-        console.error(e.code)
-      }
-    }
-  }
-);
+
 
 
 
@@ -80,15 +65,45 @@ const authProvider = new RefreshingAuthProvider(
 
 async function main() {
 
+  const server = registerServerHandler(process.env.PORT)
+
+  registerApiConnectTwitch(appContext, server)
+
   const channels = await getBotUsers(appContext)
 
   const tokenData = await getTwitchTokenData(
     process.env.FRONTEND_URL,
     process.env.BACKEND_URL,
+    process.env.BOT_URL,
     process.env.BOT_NAME,
     process.env.STRAPI_API_KEY,
+    process.env.TWITCH_CLIENT_ID,
+    process.env.TWITCH_CLIENT_SECRET,
   )
 
+  const authProvider = new RefreshingAuthProvider(
+    {
+      clientId: process.env.TWITCH_CLIENT_ID,
+      clientSecret: process.env.TWITCH_CLIENT_SECRET,
+      onRefresh: async (userId, newTokenData) => {
+        try {
+          await setTwitchTokenData(
+            process.env.BACKEND_URL,
+            process.env.STRAPI_API_KEY,
+            userId,
+            newTokenData,
+          )
+        } catch (e) {
+          console.error('error while refreshing twitch token')
+          console.error(e)
+          console.error(e.options.body)
+          console.error(e.code)
+        }
+      }
+    }
+  );
+
+  console.log(`main'd and we ahve some token data ${JSON.stringify(tokenData)}`)
 
 
   await authProvider.addUser(process.env.BOT_NAME, tokenData, ['chat']);
@@ -98,6 +113,7 @@ async function main() {
     authProvider, 
     channels
   });
+
 
   const auctions = registerAuctions(appContext, channels, chatClient)
 
